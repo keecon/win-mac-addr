63da5be80f55c9bf963d714b8750a97faf749d13
smilejp <smile.wjp@gmail.com>
2022-12-25 11:34:40 +0900

    feat: 콘솔 출력 결과를 파일에 저장하도록 추가 (#2)
    Co-authored-by: smilejp <smilewjp@keecon.com>

--- main.go
+++ main.go
@@ -21,6 +21,10 @@ func main() {
 		panic("GetAdaptersAddresses: " + err.Error())
 	}
 
+	f1, err := os.Create("output.txt")
+	checkError(err)
+	defer f1.Close()
+
 	for _, v := range addrs {
 		if v.FirstGatewayAddress == nil || v.PhysicalAddress() == nil {
 			continue
@@ -30,7 +34,19 @@ func main() {
 		fmt.Printf("AdapterName: %s\n", v.AdapterName())
 		fmt.Printf("Description: %s\n", v.Description())
 		fmt.Printf("PhysicalAddress: %v\n", net.HardwareAddr(v.PhysicalAddress()))
+
+		fmt.Fprintf(f1, "Name: %s\n", v.FriendlyName())
+		fmt.Fprintf(f1, "AdapterName: %s\n", v.AdapterName())
+		fmt.Fprintf(f1, "Description: %s\n", v.Description())
+		fmt.Fprintf(f1, "PhysicalAddress: %v\n", net.HardwareAddr(v.PhysicalAddress()))
 	}
 
 	bufio.NewScanner(os.Stdin).Scan()
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
